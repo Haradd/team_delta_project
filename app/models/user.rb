776f0ba31ec342051ae522c84d9a27705c8c6d80
Magda Mieczynska <magda.mieczynska@gmail.com>
2017-08-11 14:53:56 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+  validates :first_name, presence: true, length: { in: 2..30 }
+  validates :last_name, presence: true, length: { min: 2 }
+  validates :phone, presence: true,
+            format: { with: /^(?:\(?\+?48)?(?:[-\.\(\)\s]*(\d)){9}\)?$/,
+            message: "only allows numbers" }
+  validates :city, presence: true
+  validates :street, presence: true
 end
