0302fe5bc49d99e5e0d5c72abe9c265ff227f727
Haradd <blazej.piaskowski@gmail.com>
2017-08-12 13:05:14 +0200

    fix Users table

--- db/migrate/20170811114311_add_additional_user_data_to_users.rb
+++ db/migrate/20170811114311_add_additional_user_data_to_users.rb
@@ -5,10 +5,6 @@ class AddAdditionalUserDataToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :phone, :string
     add_column :users, :city, :string
     add_column :users, :street, :string
-    add_column :users, :house_number, :string
-    add_column :users, :flat_number, :string
-    add_column :users, :post_code, :string
-    add_column :users, :is_nanny, :boolean, default: false
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811114311) do
+ActiveRecord::Schema.define(version: 20170811143056) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -30,10 +30,6 @@ ActiveRecord::Schema.define(version: 20170811114311) do
     t.string "phone"
     t.string "city"
     t.string "street"
-    t.string "house_number"
-    t.string "flat_number"
-    t.string "post_code"
-    t.boolean "is_nanny", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
