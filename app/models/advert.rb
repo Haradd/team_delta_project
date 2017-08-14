a97a2d8fc7ae1f621458d8659539051acabdbc33
Magda Mieczynska <magda.mieczynska@gmail.com>
2017-08-14 14:11:51 +0200

    Add validation for phone uniqueness

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,8 +2,8 @@ class Advert < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :description, presence: true
+  # validates :description, presence: true
   validates :city, presence: true
   validates :street, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { in: 2..30 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :phone, presence: true,
+                    uniqueness: true,
                     format: { with: /\A\d{9}\z/,
                               message: "please type in format: '123456789'" }
   validates :city, presence: true
