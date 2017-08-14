c7c4c6d3e2344bbd890fb31b8e621dcf80ce1d7d
Haradd <blazej.piaskowski@gmail.com>
2017-08-15 00:39:50 +0200

    rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   validates :city, presence: true
   validates :street, presence: true
 
-  has_attached_file :avatar, styles: {medium: "270x270#", small: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "270x270#", small: "100x100#" },
+                             default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
 end
