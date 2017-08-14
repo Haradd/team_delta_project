aef77f78545aab50a4a1fa32ff04645093861645
Haradd <blazej.piaskowski@gmail.com>
2017-08-15 00:36:55 +0200

    add Avatar to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ApplicationRecord
                               message: "please type in format: '123456789'" }
   validates :city, presence: true
   validates :street, presence: true
+
+  has_attached_file :avatar, styles: {medium: "270x270#", small: "100x100#" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814133952) do
+ActiveRecord::Schema.define(version: 20170814143619) do
 
   create_table "adverts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 20170814133952) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_adverts_on_user_id"
-  end
-
-  create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "address"
-    t.float "latitude", limit: 24
-    t.float "longitude", limit: 24
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -50,10 +42,10 @@ ActiveRecord::Schema.define(version: 20170814133952) do
     t.string "phone"
     t.string "city"
     t.string "street"
-    t.string "house_number"
-    t.string "flat_number"
-    t.string "post_code"
-    t.boolean "is_nanny", default: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
