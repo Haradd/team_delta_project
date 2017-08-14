b0c68228e4aa9989a51d80036a9fbd80e9116ba9
Haradd <blazej.piaskowski@gmail.com>
2017-08-15 00:37:28 +0200

    remove phone uniqness validation on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { in: 2..30 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :phone, presence: true,
-                    uniqueness: true,
                     format: { with: /\A\d{9}\z/,
                               message: "please type in format: '123456789'" }
   validates :city, presence: true
