eda65e1718fbc6796c7a7361e4dcd42b799b5a27
@raddy86 <raddy.matuszak@gmail.com>
2017-08-16 00:23:02 +0200

    Improve advert validation

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,12 +1,14 @@
 class Advert < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, lenght: { in: 5..40,
+  validates :title, presence: true, length: { in: 5..40,
                               message: "Title to long, please use description field" }
-  validates :description, presence: true, lenght: { in: 5..150 }
+  validates :description, presence: true, length: { in: 5..300,
+                              message: "Only 300 caracters allowed!" }
   validates :city, presence: true
   validates :street, presence: true
   validates :phone, presence: true,
                     format: { with: /\A\d{9}\z/,
                               message: "please type in format: '123456789'" }
+  validates :created_at, presence: true
 end
