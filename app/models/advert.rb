cfa2f95877202b76fab8b26c64f468090272e9bb
@raddy86 <raddy.matuszak@gmail.com>
2017-08-15 13:18:06 +0200

    Add lenght validation to title andf description

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,8 +1,9 @@
 class Advert < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, lenght: { in: 5..40,
+                              message: "Title to long, please use description field" }
+  validates :description, presence: true, lenght: { in: 5..150 }
   validates :city, presence: true
   validates :street, presence: true
   validates :phone, presence: true,
