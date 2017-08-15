70b9a4ea78902f623a8e0b092d4c5a7658f002c8
Magda Mieczynska <magda.mieczynska@gmail.com>
2017-08-15 17:01:16 +0200

    Add description to Factory Advert

--- spec/factories/adverts.rb
+++ spec/factories/adverts.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
   factory :advert do
     association :user, factory: :user
-    title { FFaker::Lorem.words(title_length).join(" ") }
+    title { FFaker::Lorem.words.join(" ") }
     city { FFaker::Address.city }
     street { FFaker::Address.street_name }
     phone { FFaker::PhoneNumberAU.mobile_phone_number.gsub!(/\s/, '').slice(1..9) }
+    description { FFaker::Lorem.sentences }
   end
 end
