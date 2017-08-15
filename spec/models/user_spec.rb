7c9a2a74f777350d6351c8b3c86e0af342fbe640
@raddy86 <raddy.matuszak@gmail.com>
2017-08-16 01:16:15 +0200

    Add Spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :phone }
+    it { is_expected.to validate_presence_of :city }
+    it { is_expected.to validate_presence_of :street }
+  end
 end
