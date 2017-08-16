86c9d2735eec838b00764d80e9e1c197e428a881
Haradd <blazej.piaskowski@gmail.com>
2017-08-16 13:51:52 +0200

    update validation fot job_type

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -10,5 +10,5 @@ class Advert < ApplicationRecord
                               message: "please type in format: '123456789'" }
 
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :job_type, presence: true;
+  validates :job_type, presence: true, inclusion: %w(Full-time Part-time);
 end
