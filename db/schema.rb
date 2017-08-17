ffd9a0648ad29b1098b3a5b90e3fa82742b22741
Haradd <blazej.piaskowski@gmail.com>
2017-08-17 11:18:32 +0200

    avatar update in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170816153736) do
 
   create_table "adverts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
--- spec/factories/adverts.rb
+++ spec/factories/adverts.rb
@@ -1,10 +1,13 @@
 FactoryGirl.define do
   factory :advert do
     association :user, factory: :user
+
     title { FFaker::Lorem.words.join(" ") }
     city { FFaker::Address.city }
     street { FFaker::Address.street_name }
     phone { FFaker::PhoneNumberAU.mobile_phone_number.gsub!(/\s/, "").slice(1..9) }
     description { FFaker::Lorem.sentence }
+
+
   end
 end
